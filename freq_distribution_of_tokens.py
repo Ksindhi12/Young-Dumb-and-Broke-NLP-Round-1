be886ece2b0f847b238b4a62fe4f9239947814ec
psomani16k <66767160+psomani16k@users.noreply.github.com>
2023-10-25 12:30:49 +0530

    Revert "Merge pull request #3 from Ksindhi12/main"
    This reverts commit 75718d153d8bfdc2e6165e3ee990a49f6dd143a9, reversing
    changes made to 57395b42a3373a67b331aa0c1fc7fc68ffe08a6a.

--- freq_distribution_of_tokens.py
+++ freq_distribution_of_tokens.py
@@ -7,22 +7,20 @@ import matplotlib.pyplot as plt
 
 def PrintFrequencyDistribution(tokens:list):
     TokenFD = nltk.FreqDist(tokens)
-    #for token in list(TokenFD.items()):
-    #   print(token[0], token[1])
-    #print("X-------------------------------------------------X-------------------------------------------------X")
+    for token in list(TokenFD.items()):
+        print(token[0], token[1])
+    print("X-------------------------------------------------X-------------------------------------------------X")
 
 def VisualiseFrequencyDistribution(tokens:list):
     mostCommonFdist = nltk.FreqDist(tokens).most_common(20)
     mostCommonFdist = pandas.Series(dict(mostCommonFdist))
-    figure, ax = plt.subplots(figsize = (10,10))
+    figure, ax = plt.pyplot.subplots(figsize = (10,10))
 
     freqDistribution_plot = seaborn.barplot(x = mostCommonFdist.index, y = mostCommonFdist.values, ax = ax)
-    plt.xticks(rotation = 30)
+    plt.pyplot.xticks(rotation = 30)
 
 def CreateWordCloud(tokens:list):
-    wordString = ""
-    wordString += " ".join(tokens)+" "
-    wordcloud = WordCloud(width = 800, height = 800, background_color = 'white', min_font_size = 10).generate(wordString)
+    wordcloud = WordCloud(width = 800, height = 800, background_color = 'white', min_font_size = 10).generate(tokens)
     plt.figure(figsize = (8, 8), facecolor = None)
     plt.imshow(wordcloud)
     plt.axis("off")
